2bebd22ef752b4c52583cb72f68514d2316840cb
rafi-coredevs <rafi_uddin@coredevs.ltd>
2023-11-17 17:31:58 +0600

    ..

--- server.js
+++ server.js
@@ -5,21 +5,12 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const app = Express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
 app.use(Express.json());
 
-// middleware
-const middleWare = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        const isValid = jwt.verify(token, process.env.SALT);
-        if (isValid) return next();
-        return res.status(401).send('Unathorized')
-
-    } catch (error) {
-        return res.status(401).send("Missing Credentials");
-    }
-}
 // MongoDB
 const productSchema = new mongoose.Schema({
     foodName: { type: String, required: true },
@@ -27,7 +18,11 @@ const productSchema = new mongoose.Schema({
     foodCategory: { type: String, required: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
-    creator: { type: String, required: true },
+    sold: { type: Number, required: true},
+    creator: { 
+        email: {type: String, required: true},
+        name: {type: String, required: true}
+     },
     origin: { type: String, required: true },
     description: { type: String, required: true },
 });
@@ -37,6 +32,11 @@ const userSchema = new mongoose.Schema({
     profile: { type: String },
     password: { type: String, required: true },
 });
+userSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.password
+    return obj
+}
 const cartSchema = new mongoose.Schema({
     user: mongoose.Types.ObjectId,
     products: { type: Array }
@@ -44,61 +44,137 @@ const cartSchema = new mongoose.Schema({
 const userModel = mongoose.model('user', userSchema);
 const productModel = mongoose.model('product', productSchema);
 
+// middleware
+const middleWare = async (req, res, next) => {
+    try {
+        const token = req.headers.cookie.split('=')[1];
+        const isValid = jwt.verify(token, process.env.SALT);
+        const user = await userModel.findById(isValid.user_id)
+        req.user = user;
+        if (user) return next();
+        return res.status(401).send('Unathorized')
+
+    } catch (error) {
+        return res.status(401).send("Missing Credentials");
+    }
+}
 
 //User section
 app.post('/sign-up', async (req, res, next) => {
-    const { email, password, profile, name } = req.body;
+    const { email, password, profile, name } = await req.body;
     let result;
     try {
-        let user = userModel.findOne({ email: email })
+        let user = await userModel.findOne({ email: email })
         if (user) {
-            return res.status(409).send("email address already Used");
+            return res.status(400).send("email address already Used");
         }
-        const cryptPassword = await bcrypt.hash(password, process.env.SALT, 12);
+        const cryptPassword = await bcrypt.hash(password, 12);
         const createdUser = new userModel({
             email, password: cryptPassword, profile, name
         });
         result = await createdUser.save();
     } catch (error) {
         console.log(error);
-        return res.status(404).send('Bad Request')
+        return res.status(400).send('Bad Request')
     }
     res.status(201).send(result)
 })
-
+app.get('/user/me', middleWare, async (req, res) => {
+    try {
+        return res.status(200).send(req.user)
+    } catch (error) {
+        return res.status(400).send('email or password not matched')
+    }
+})
 app.post('/login', async (req, res, next) => {
     const { email, password } = req.body;
     try {
         let user = await userModel.findOne({ email: email });
+        console.log(user);
         if (user) {
             const decryptPass = await bcrypt.compare(password, user.password);
-            console.log(decryptPass);
             const token = jwt.sign({ user_id: user._id, email }, process.env.SALT)
-            return res.cookie("token", token).send(decryptPass);
+            if (decryptPass) {
+                res.cookie('cafeu', token);
+                return res.status(200).send(user)
+            } else {
+                res.status(401).send('email or passwoed not Found')
+            }
+        } else {
+            return res.status(400).send('email or passwoed not Found')
         }
     } catch (error) {
-        return res.status(404).send('email or password not matched')
+        return res.status(400).send('email or password not matched')
+    }
+});
+app.post('/logout',(req, res)=>{
+    try{
+
+        res.clearCookie('cafeu');
+        res.status(200).send("Logout Successfull");
+    }catch(error){
+        res.status(400).send('Bad Request');
     }
 })
 // Products section;
+// app.get('/products', async (req, res, next) => {
+    
+//     let result;
+//     try {
+//         if(req.query.sortBy) {
+//             const sortBy = req.query.sortBy
+//             const [field, order] = sortBy.split('|');
+//             console.log(field, order);
+//             result = await productModel.find({}).sort({ field : order === 'asc' ? 1 : -1 }).limit(req.query.limit || 6);
+//             return res.send(result);
+//         }
+
+//         result = await productModel.find({}).limit(req.query.limit || 6);
+//         return res.send(result);
+//     } catch (error) {
+//         console.log(error)
+//         return res.status(400).send("bad Request");
+//     }
+// });
+
 app.get('/products', async (req, res, next) => {
-    console.log(req.headers)
+    try {
+        let query = {};
+        let sortOptions = {};
+
+        // Check if sortBy is provided in the query parameters
+        if (req.query.sortBy) {
+            const [field, order] = req.query.sortBy.split('|');
+            console.log(field, order);
+            sortOptions[field] = order === 'asc' ? 1 : -1;
+        }
+
+        // Use the constructed query and sort options in the productModel.find method
+        const result = await productModel.find(query).sort(sortOptions).limit(req.query.limit || 6);
+
+        return res.send(result);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send("Bad Request");
+    }
+});
+
+app.get('/products/me', middleWare, async (req, res, next) => {
+    const user = req.user;
     let result;
     try {
-        result = await productModel.find();
+        result = await productModel.find({ "creator.name": user.name, "creator.email": user.email }).exec();
     } catch (error) {
         return res.status(400).send("bad Request");
     }
     res.send(result)
 });
-
-app.post('/products', middleWare, async (req, res,) => {
+app.post('/products', async (req, res,) => {
     const { foodName, foodImage, foodCategory, price, quantity, creator, origin, description } = req.body;
     let result;
-
     try {
         const createdProduct = new productModel({
-            foodName, foodImage, foodCategory, price, quantity, creator, origin, description
+            foodName, foodImage, foodCategory, price, quantity,sold:0, creator, origin, description
         })
         result = await createdProduct.save();
     } catch (error) {
@@ -149,7 +225,6 @@ app.get('/cart', middleWare, async (req, res, next) => {
         if (error) {
             return res.send('Bad Request');
         } else {
-            console.log(decoded)
             return {
                 userId: decoded.user_id,
                 email: decoded.email
